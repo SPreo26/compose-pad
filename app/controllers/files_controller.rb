252c04e70d5293e07e3930c2e9c2e2ca37356441
Spreo26 <Spreo26@users.noreply.github.com>
2015-12-21 21:46:57 -0800

    added some prelim code to files controller

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,6 +1,32 @@
 class FilesController < ApplicationController
 
+  #before_action
+  #def authenticate_admin
+
   def index
+
+    if current_user
+      @files = File.where(user_id: current_user)
+    else
+      flash[:danger]="You need to sign in to view your files!"
+      redirect_to "/users/sign_in"
+    end
+
+  end
+
+  def show
+
+    if current_user
+      @file = File.find_by(id: params[:id], user_id: current_user)
+      if @file
+        #render?????
+      else
+        flash[:danger]="File id #{params[:id]} not found under this user need to sign in to view your files!"
+      end
+    else      
+      flash[:danger]="You need to sign in to view your files!"
+      redirect_to "/users/sign_in"
+    end
 
   end
 
