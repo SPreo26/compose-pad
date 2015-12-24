d2ce04d01427976126a1be0950b3d66103b63446
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2015-12-24 04:07:48 -0800

    built mockup html checkbox array for note pitch vs time-position (more eye-pleasing js will be used in the future); built save button for checkbox array form in nav bar (used a custom js function), this save button does not save to the database yet

--- app/controllers/note_files_controller.rb
+++ app/controllers/note_files_controller.rb
@@ -4,14 +4,12 @@ class NoteFilesController < ApplicationController
   #def authenticate_admin
 
   def index
-
     if current_user
       @files = NoteFile.where(user_id: current_user)
     else
       flash[:danger]="You need to sign in to view your files!"
       redirect_to "/users/sign_in"
     end
-
   end
 
   def show
@@ -19,7 +17,8 @@ class NoteFilesController < ApplicationController
     if current_user
       @file = NoteFile.find_by(id: params[:id], user_id: current_user)
       if @file
-        #render?????
+        @file.update(file_is_open:true)
+        redirect_to "/workspace"
       else
         flash[:danger]="File id #{params[:id]} not found under this user need to sign in to view your files!"
         redirect_to "/"
@@ -31,8 +30,20 @@ class NoteFilesController < ApplicationController
 
   end
 
-  def open_files
+  def update
+    redirect_to "/workspace"
+  end
 
+  def open_files
+    @action_is_open_files = true
+    @max_measure = 2
+    @beats_per_measure = 4
+    @divisions_per_beat = 4
+    @max_octave = 4
+    @min_octave = 3
+    @max_tone = "B"
+    @min_tone = "C"
+    @tones_array = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"].reverse#reverse as table in view is built from top down
   end
 
 
