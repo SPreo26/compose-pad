a4680b8b4b770e57a48b09202cf822285b2b6510
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2015-12-29 03:11:23 -0800

    redid routes as custom, non-restful - easier to understand; almost worked out styling for index page; index page will soon be able to open/delete/rename a file

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,29 @@ Rails.application.routes.draw do
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
-  get '/workspace/', to: 'note_files#opened_files'
+  get '/my_files/', to: 'note_files#index'#leads to an index page from which you can open,rename,delete
+  get '/note_files/open_rename_or_delete_files'
+  get '/note_files/open_files', to: 'note_files#open_files'
+    patch 'note_files/', to: 'note_files#rename_files'
+  delete 'note_files/', to: 'note_files#delete_files'
+  get '/workspace/', to: 'note_files#workspace'
+
+
+  get '/note_files/new', to: 'note_files#new'
+  post '/note_files/', to: 'note_files#create'
+
+  patch '/note_files/:id/', to: 'note_files#save'
+  get '/note_files/save/', to: 'note_files#save_all'
+
+
+
+  get '/note_files/close_all', to: 'note_files#close_all'
+  
+  get '/note_files/delete', to: 'note_files#delete_index'
+  
+  
+
+
   resources :note_files
 
   # The priority is based upon order of creation: first created -> highest priority.
