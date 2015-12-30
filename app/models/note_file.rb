dc0ee9b1e623f15a758f9b2291af8785e38098b0
Spreo26 <Spreo26@users.noreply.github.com>
2015-12-30 03:52:51 -0800

    Changed title of My Files panel to simply My Files; no longer using col-md class because of disappearing issues on resize - instead setting max height and width of panel - centering via table and table-cell; still need to add footer to panels for buttons; still need to set overflow scroll for my files panel

--- app/models/note_file.rb
+++ app/models/note_file.rb
@@ -3,5 +3,6 @@ class NoteFile < ActiveRecord::Base
   has_many :loaded_notes
 
   validates :user_id, presence: {message: "You must be logged in to create a file!"}
+  validates :name, length: {maximum: 30}
   validates :name, uniqueness: {scope: :user_id, message: "This file name already exists! Please choose another."} 
 end
