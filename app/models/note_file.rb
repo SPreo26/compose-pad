685ceed354aeb24be212785a68fb4cdec53327f6
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2015-12-31 17:02:40 -0800

    improved styling of create-file panel; index panel body no has a scroll bar; alerts now show up below navbar, without an extra margin on the bottom

--- app/models/note_file.rb
+++ app/models/note_file.rb
@@ -3,6 +3,7 @@ class NoteFile < ActiveRecord::Base
   has_many :loaded_notes
 
   validates :user_id, presence: {message: "You must be logged in to create a file!"}
-  validates :name, length: {maximum: 30}
+  validates :name, length: {minimum: 1, maximum: 30, too_short: "File name cannot be blank!", too_long: "File name cannot exceed 30 characters!"}
+
   validates :name, uniqueness: {scope: :user_id, message: "This file name already exists! Please choose another."} 
 end
