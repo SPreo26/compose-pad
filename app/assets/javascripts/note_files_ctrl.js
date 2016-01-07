7060d42fdfea95335004ce35a3a72aa7a6093d1c
Spreo26 <Spreo26@users.noreply.github.com>
2016-01-07 03:26:15 -0800

    file list is now angular controlled, but have trouble passing  to plain js functions that style the file list

--- app/assets/javascripts/note_files_ctrl.js
+++ app/assets/javascripts/note_files_ctrl.js
@@ -6,14 +6,40 @@
     $scope.setup = function() {
       $http.get("/api/v1/note_files.json").then(function(response) {
         $scope.noteFiles = response.data;
-        console.log($scope.noteFiles);
-      });
+        for (var i=0; i<$scope.noteFiles.length; i++){
+          $scope.noteFiles[i].selected = false;
+        }
+        // console.log($scope.noteFiles);
+      },
+        function(error){
+          console.log(error.data)
+        });
 
+    }
+
+    $scope.selectedFruits = function selectedFiles() {
+    return filterFilter($scope.noteFiles, { selected: true });
+    };
+
+
+    $scope.renameFile = function(index){
+      var id=$scope.noteFiles[index].id;
+      var rename_data = {
+        name: noteFiles[index].name,
+        rename: true
+      };
+      $http.patch('api/vi/note_files', rename_data).then(function(response) {
+        $scope.noteFiles = response.data;
+        // console.log($scope.noteFiles);
+      },
+        function(error){
+          console.log(error.data)
+        });
     }
 
     // $scope.deleteShow = function(index){
     //   var id = $scope.shows[index].id;
-    //   $http.delete('/api/v1/shows/'+id, $scope.shows[index]).then(
+    //   $http.delete('/api/v1/shows/'+id).then(
     //     function(response){
     //     console.log(response.data);
     //     $scope.shows.splice(index,1);
@@ -21,7 +47,6 @@
     //     function(error){
     //     console.log(error.data);
     //   })
-     
     // }
 
     // $scope.addShow = function(newDatetime, newArtistName, newVenue, newCity, newRegion, newCountry) {
@@ -44,7 +69,7 @@
     // }
 
 
-    // $scope.deleteShow = function(index){
+    // $scope.deleteFile = function(index){
     //   var id = $scope.shows[index].id;
     //   $http.delete('/api/v1/shows/'+id, $scope.shows[index]).then(
     //     function(response){
