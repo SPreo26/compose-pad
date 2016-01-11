b6840e44d231bea71a30184afb6c2dce00397951
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2016-01-10 20:10:47 -0800

    proceding with angular and my_files, but can't figure out what event will delay styling before file_line id's are set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,32 @@ class ApplicationController < ActionController::Base
 
   #this prevents a redirect loop after signing in as root is set to the sign-in page
   def after_sign_in_path_for(resource_or_scope)
-    if NoteFile.find_by(file_open: true)
-      "/workspace"
-    else
-      "/my_files/"
+
+      if NoteFile.find_by(file_open: true)
+        # puts "WORKSPACE!!!!!"
+        "/workspace"
+      else
+        "/my_files/"
+      end
+
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+      "/users/sign_in"
+
+  end
+
+  def authenticate_user
+
+    if !current_user
+      flash[:danger] = sign_in_message()
+      redirect_to :root
     end
+
+  end
+
+  def sign_in_message
+   "Please sign in before you proceed!"
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228083414) do
+ActiveRecord::Schema.define(version: 20160110181638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
