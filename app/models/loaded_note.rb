c39430141dceca8ba3150e50b600b28fcdf63fb2
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2016-01-20 03:21:42 -0800

    now duplicate-time notes are removed; need to add ability to actually play chords at some point; worskpace matrix expanded to as far as screen can fit and ranges from c4 to d6; different users can use api now (still need to test this)

--- app/models/loaded_note.rb
+++ app/models/loaded_note.rb
@@ -1,3 +1,18 @@
 class LoadedNote < ActiveRecord::Base
   belongs_to :note_file
+
+  def self.get_json_no_time_duplicates(file)
+    
+    notes=file.loaded_notes.order(:start_index)
+    new_notes=[]
+    notes.each do |note|
+      if new_notes.empty? || new_notes.last.start_index != note.start_index
+        new_notes<<note
+      end
+    end
+
+    return new_notes.as_json
+
+  end
+  
 end
