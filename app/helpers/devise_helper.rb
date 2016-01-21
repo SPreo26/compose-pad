43f5af9ef7a2fd64ea187e0e6c9812e11b250ac0
Spreo26 <Spreo26@users.noreply.github.com>
2016-01-20 23:01:57 -0800

    fixed bug that prevented flash messages; deleted some devise flash messages from /config/locales/devise.en.yml

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,20 +1,20 @@
 module DeviseHelper
-  def devise_error_messages!
-    # p "BBBBBBBBBBB"
-    # return '' if resource.errors.empty?
+  # def devise_error_messages!
+  #   # p "BBBBBBBBBBB"
+  #   # return '' if resource.errors.empty?
 
-    # messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    # p "ASFASFASFASFASF"
-    # p messages
-    # messages.delete_if{|msg| msg=="Signed in successfully."}
-    # html = <<-HTML
-    # <div class="alert alert-error alert-danger"> <button type="button"
-    # class="close" data-dismiss="alert">x</button>
-    #   #{messages}
-    # </div>
-    # HTML
+  #   # messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+  #   # p "ASFASFASFASFASF"
+  #   # p messages
+  #   # messages.delete_if{|msg| msg=="Signed in successfully."}
+  #   # html = <<-HTML
+  #   # <div class="alert alert-error alert-danger"> <button type="button"
+  #   # class="close" data-dismiss="alert">x</button>
+  #   #   #{messages}
+  #   # </div>
+  #   # HTML
 
-    # html.html_safe
-  end
+  #   # html.html_safe
+  # end
 
 end
