ef792a93e23b1121a5b6f7ce04392b2b67736507
Spreo26 <Spreo26@users.noreply.github.com>
2016-03-16 23:39:20 -0700

    config.serve_static_assets set to true - attempting to make asset pipeline worj on heroku

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module ComposePad
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    # config.serve_static_assets = false
+    config.serve_static_assets = true
     # Dir.glob("#{Rails.root}/vendor/assets/javascripts/**/").each do |path|
     # config.assets.paths << path
     #end
