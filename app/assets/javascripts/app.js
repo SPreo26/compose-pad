1641dbce9f6f964e6903cb1cc2ed1871821bdd04
Spreo26 <Spreo26@users.noreply.github.com>
2016-06-27 03:32:05 -0700

    put ng-app in body of application.erb.html; implemented two ng-controllers: noteFilesCtrl and workspaceCtrl; the latter only has the ng-init='setup' to fetch workspace data, nested ng-repeat not yet iplemented for workspace: tabs, then for each tab (open file) rows and columns

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,8 +1,6 @@
 (function() {
   "use strict";
 
-  angular.module("appNoteFiles", []);
-
-  angular.module("appWorkspace", []);
+  angular.module("app", ["ngAnimate"]);
 
 }());
--- app/assets/javascripts/note_files_ctrl.js
+++ app/assets/javascripts/note_files_ctrl.js
@@ -1,6 +1,6 @@
 (function () {
   "use strict";
-  var app=angular.module("appNoteFiles", ["ngAnimate"]);
+  var app=angular.module("app");
   app.directive(
     "repeatComplete",
     function( $rootScope ) {
