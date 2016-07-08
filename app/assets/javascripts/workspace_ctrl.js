734af71d31db0ae1fd81d60ae98c2cb804a70bb8
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2016-07-08 01:56:55 -0700

    added ng-if to only show tabs if file is open; still need to expand angular workspace_ctrl to encompass the dropdown menu to save and close files via angular; added scope.closeFile function which works but cannot be invoked in view until the former is done; added funciton .loadNotesIntoMatrixModel which is now invoked in .setup for workspace_ctrl, but the function is yet empty; it will be filled with code to turn workspace matrix into an angular model for every toggleable note on the screen

--- app/assets/javascripts/workspace_ctrl.js
+++ app/assets/javascripts/workspace_ctrl.js
@@ -9,11 +9,12 @@
       $http.get("/api/v1/note_files/open").then(function(response) {
         $scope.workspaceData = response.data;
         $scope.files = $scope.workspaceData.files;
-        $scope.openFiles = $scope.files.filter($scope.returnOpenFiles)
+        // $scope.openFiles = $scope.files.filter($scope.returnOpenFiles)
         $scope.allBeatCells = $scope.workspaceData.beats;
         $scope.allDivisions = $scope.workspaceData.divisions;
         $scope.pitchesInWorkspace = $scope.workspaceData.pitches_in_workspace;
         $scope.timeConstants=$scope.workspaceData.time_constants;
+        $scope.loadNotesIntoMatrixModel
 
         loadMidi();
 
@@ -22,43 +23,37 @@
           console.log(error.data);
         }
       )
-    }
+    };
 
-    $scope.returnOpenFiles = function(files){
-      if (files.file_open){
-        return true
-      }
-      else {
-        return false
-      }
-    }
+    // $scope.returnOpenFiles = function(files){
+    //   if (files.file_open){
+    //     return true
+    //   }
+    //   else {
+    //     return false
+    //   }
+    // }
+    $scope.loadNotesIntoMatrixModel = function() {
+    };
+
+    $scope.closeFile = function(file){
+      file.file_open = false;
+      return;
+    };
 
     $scope.thereIsNoteAtDivisionAndPitch = function(file,division,pitch){
       var notes = file.notes;
       var i;
       for (i=0;i<notes.length; i++){
         if (notes[i].start_index == division && notes[i].pitch == pitch) {
-          return true
+          return true;
         }
       }
-      return false
-    }
+      return false;
+    };
 
     window.$scope = $scope;
   
     });
-
-  // app.run([
-  //   '$window',
-  //   '$rootScope',
-  //   function($window, $rootScope) {
-  //     $window.addEventListener('beforeunload', function() {
-  //       if(window.location.href.indexOf("my_files") > -1 && document.activeElement.id.indexOf("file_text")> -1 || true) {
-  //         var index = parseInt(document.activeElement.id.slice("file_text") );
-  //         $scope.renameFile(index);
-  //       }
-  //     });
-  //   }
-  // ]);
 
 }());
