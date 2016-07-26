b61f28ad01093c9dea307a5302a8d24ff4971777
Spreo26 <Spreo26@users.noreply.github.com>
2016-07-26 04:40:01 -0700

    play button and close menu item in workspace are now both tied to angular; close sends data to api; ng-click on each notebox executes a function that prevents more than one pitch to exist at a given start index - for now this makes user unable to enter intervals/chords

--- app/assets/javascripts/file_playing.js
+++ app/assets/javascripts/file_playing.js
@@ -1,43 +1,42 @@
 var  midi_loaded=false;
 
-window.onload = function() {
-  if(document.URL.indexOf("workspace") > -1){
-    $.ajax({url: "/api/v1/note_files/open", success: function(result){
-        workspaceData=result;
-        console.log(result);
-        MIDI.loadPlugin({
-        soundfontUrl: "./soundfont/",
-        instrument: "acoustic_grand_piano",
-          onprogress: function(state, progress) {
-            console.log(state, progress);
-          },
-          onsuccess: function() {
-            midi_loaded=true;
-            console.log("Midi loaded")
-          }
-        });
-      },
-      error: function(result){
-        console.log(result);
-      }
-    });
-  }
-}
+// window.onload = function() {
+//   if(document.URL.indexOf("workspace") > -1){
+//     $.ajax({url: "/api/v1/note_files/open", success: function(result){
+//         workspaceData=result;
+//         console.log(result);
+//         MIDI.loadPlugin({
+//         soundfontUrl: "./soundfont/",
+//         instrument: "acoustic_grand_piano",
+//           onprogress: function(state, progress) {
+//             console.log(state, progress);
+//           },
+//           onsuccess: function() {
+//             midi_loaded=true;
+//             console.log("Midi loaded")
+//           }
+//         });
+//       },
+//       error: function(result){
+//         console.log(result);
+//       }
+//     });
+//   }
+// }
 
-function loadMidi()
-  {
-    MIDI.loadPlugin({
-        soundfontUrl: "./soundfont/",
-        instrument: "acoustic_grand_piano",
-          onprogress: function(state, progress) {
-            console.log(state, progress);
-          },
-          onsuccess: function() {
-            midi_loaded=true;
-            console.log("Midi loaded")
-          }
-        });
-  }
+function loadMidi() {
+  MIDI.loadPlugin({
+      soundfontUrl: "./soundfont/",
+      instrument: "acoustic_grand_piano",
+        onprogress: function(state, progress) {
+          console.log(state, progress);
+        },
+        onsuccess: function() {
+          midi_loaded=true;
+          console.log("Midi loaded")
+        }
+      });
+};
 
 function findFileById(workspaceData,id) {
   for (i=0;i<workspaceData.files.length;i++){
@@ -45,56 +44,82 @@ function findFileById(workspaceData,id) {
       return workspaceData.files[i];
     }
   }
-}
+};
 
-function playFile(file) {
+function playFile(file, divisions, octaveTones) {
   if (midi_loaded) {
 
-    var date = new Date();
-    var startTime = date.getTime();
-    var timeIncrement = 250;
-    var delay=.500;
-    var note_index = 0
+    if (file.matrix) { 
+      var notesOrderedByStartIndex = extractNotesFromMatrixOrderedByStartIndex(file.matrix, divisions)
+      var date = new Date();
+      var startTime = date.getTime();
+      var timeIncrement = 250;
+      var delay=.500;
 
-    for(i=0;i<workspaceData.divisions.length;i++){
-      var note = file.notes[note_index];
-      if (note.start_index ==
-        workspaceData.divisions[i]){
-        currentMidiNote=convertNoteToMidiNote(note);
-        note_index++;
-      }else{
-        currentMidiNote=null;
-      }
-      var nextPlayTime=startTime+timeIncrement*i;
-      // if (file.notes[i]){
-        // playNote(convertNoteToMidiNote(file.notes[0]),delay)
-        //   setTimeout( function() {
-        //     playNote(convertNoteToMidiNote(file.notes[0]),delay);
-        //   }, 1000)
-      // }
-      while (true){
-        date = new Date();
-        time = date.getTime();
-        if (time >= nextPlayTime){
-          if (currentMidiNote){
-            playNote(currentMidiNote,delay);
-          }
-          break
+      var i;
+      for(i=0;i<divisions.length;i++){
+        var pitch = notesOrderedByStartIndex[divisions[i]];
+        if (pitch){
+          currentMidiNote=convertNoteToMidiNote(pitch, octaveTones);
+        } 
+        else {
+          currentMidiNote=null;
         }
+        var nextPlayTime=startTime+timeIncrement*i;
+        // if (file.notes[i]){
+          // playNote(convertNoteToMidiNote(file.notes[0]),delay)
+          //   setTimeout( function() {
+          //     playNote(convertNoteToMidiNote(file.notes[0]),delay);
+          //   }, 1000)
+        // }
+        while (true){
+          date = new Date();
+          time = date.getTime();
+          if (time >= nextPlayTime){
+            if (currentMidiNote){
+              playNote(currentMidiNote,delay);
+            }
+            break
+          }
+        }
+        if (i==notesOrderedByStartIndex["lastNoteDivisionIndex"]){
+          break;
+        }  
       }
-      if (note_index>=file.notes.length){
-        break;
-      }  
+
     }
   }
-}
+};
+
+function extractNotesFromMatrixOrderedByStartIndex(matrix, divisions){
+  var notesOrderedByStartIndex = {};
+  var i;
+  for (i=0;i<divisions.length;i++){
+    notesOrderedByStartIndex[divisions[i]] = null;
+    for (pitch in matrix){
+      if (matrix[pitch][divisions[i]]){
+        notesOrderedByStartIndex[divisions[i]] = pitch; //example: notesOrderedByStartIndex["1.2.1"]="C#"
+        notesOrderedByStartIndex["lastNoteDivisionIndex"]=i;
+        break; //will only extract one pitch for a giving start index
+      };
+    };   
+  };
+  return notesOrderedByStartIndex; 
+};
 
 
-function convertNoteToMidiNote(note){
-  octave_notes=workspaceData["octave_tones"];
-  note_index_in_octave=octave_notes.indexOf(note.tone);
-  return note.octave*octave_notes.length+note_index_in_octave;
-}
+function convertNoteToMidiNote(pitch, octaveTones){
+  //need a new js function to figure out pitch.tone and pitch.octave; in the new js function, use use a single pitch like C#4 to find a number via .match(/\d+/)[0] then find index of this number and everything before that is pitch
+  pitch=extractOctaveAndToneFromPitch(pitch);
+  note_index_in_octave=octaveTones.indexOf(pitch.tone);
+  return pitch.octave*octaveTones.length+note_index_in_octave;
+};
+
+function extractOctaveAndToneFromPitch(pitch){
+  octave = pitch.match(/\d+/)[0];
+  tone = pitch.slice(0,pitch.indexOf(octave));
+  return {octave: octave, tone: tone};
+};
 
 
 function playNote(midiNote,delay) {
@@ -104,4 +129,4 @@ var note = midiNote; // the MIDI note
       MIDI.setVolume(0, 127);
       MIDI.noteOn(0, note, velocity, 0);//last zero is starting delay, i.e. start right away
       MIDI.noteOff(0, note, delay);
-}
+};
