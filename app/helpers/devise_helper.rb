b3ed5b4ec211fee4ae337a9ab741af151aa0d621
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2016-07-28 15:14:46 -0700

    removed a line in devise helper which was causing an error when an validation error message was supposed to be flashed; increased margin for sign-up button

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,8 +3,7 @@ module DeviseHelper
     return '' if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    p messages
-    messages.delete_if{|msg| msg=="Signed in successfully."}
+    
     html = <<-HTML
     <div class="alert alert-error alert-danger"> <button type="button"
     class="close" data-dismiss="alert">x</button>
