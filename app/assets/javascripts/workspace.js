c8d9e00d75c47506bfda14b17219b5bd2c3a2b27
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MBP.hsd1.ca.comcast.net>
2016-07-28 01:54:46 -0700

    finished d3 notes with pitch text; made height adjustment in adjustMatrixMaxHeightToWindowHeight() function

--- app/assets/javascripts/workspace.js
+++ app/assets/javascripts/workspace.js
@@ -1,14 +1,15 @@
 // function hexCode(d) { return d.charCodeAt(0).toString(16); }
 
-// window.onload = function() {
+window.adjustMatrixMaxHeightToWindowHeight =function(adjustment){
+    if (matrix_div_created){
+      var bodyPaddingAndMarginTop = parseInt($(document.body).css("padding-top")+$(document.body).css("margin-top"));
+      var offsetTop = Math.max(bodyPaddingAndMarginTop,$('.matrix-table-container').offset().top);
+      var offsetBottom = $('.matrix-labels-controls-container').outerHeight(true) - $('.matrix-table-container').outerHeight(true)-adjustment;
+      var maxHeight = $(document).height() - offsetTop - offsetBottom;
+      $('.matrix-table-container').css('max-height',maxHeight);
+    }
+  }
 
-//       var svg = d3.select("body").append("svg")
-//         .attr("width", 5)
-//         .attr("height", 5);
-//       var rect = svg.append("rect");
-//       rect.attr("width",5);
-//       rect.attr("hieght",5);
-//       rect.attr("fill","red");
-//       rect.attr("id","rect");
-
-// }
+  $(window).resize(function() {
+    adjustMatrixMaxHeightToWindowHeight(0);
+  });
